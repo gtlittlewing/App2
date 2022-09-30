7b60600d852f15c1321b97d68bf955b51c397095
gtlittlewing <1787665+gtlittlewing@users.noreply.github.com>
2022-09-30 15:30:30 +0900

    プロジェクト ファイルを追加します。

---
+++ App2/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml;
+using WinUIEx;
+
+namespace App2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            m_window.Activate();
+        }
+
+        private WindowEx m_window;
+    }
+}
---
+++ App2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using WinUIEx;
+
+namespace App2
+{
+    public sealed partial class MainWindow : WindowEx
+    {
+        private AcrylicSystemBackdrop AcrylicBackdrop { get; init; } = new AcrylicSystemBackdrop();
+        private MicaSystemBackdrop MicaBackdrop { get; init; } = new MicaSystemBackdrop();
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                this.ExtendsContentIntoTitleBar = true;
+                this.SetTitleBar(this.CustomAppTitleBar);
+            }
+            else
+            {
+                CustomAppTitleBar.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Backdrop_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.Backdrop = ((ComboBox)sender).SelectedIndex switch
+            {
+                0 => AcrylicBackdrop,
+                1 => MicaBackdrop,
+                _ => null,
+            };
+        }
+
+        private void Theme_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LayoutRoot.RequestedTheme = ((ComboBox)sender).SelectedIndex switch
+            {
+                1 => ElementTheme.Dark,
+                2 => ElementTheme.Light,
+                _ => ElementTheme.Default,
+            };
+        }
+    }
+}
